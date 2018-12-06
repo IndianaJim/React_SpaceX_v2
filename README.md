307bd79269724146cb6ee1c920ab0af33d3a60df
indianajim <jimrunkel@gmail.com>
2018-12-06 17:12:36 -0500

    initial setup new repo cloned from v1

---
+++ README.md
@@ -0,0 +1,4 @@
+"# React_SpaceX" 
+
+deployed to Heroku at:
+https://peaceful-caverns-25011.herokuapp.com/
---
+++ build/precache-manifest.9b051b465182d713be7a541b89b4fe86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88148b18cb9eec4ef9045e15af8c5934",
+    "url": "/static/media/logo.88148b18.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7b6f50d334751fa00c0",
+    "url": "/static/js/main.e7b6f50d.chunk.js"
+  },
+  {
+    "revision": "1da9aef565efaccc8d26",
+    "url": "/static/js/1.1da9aef5.chunk.js"
+  },
+  {
+    "revision": "e7b6f50d334751fa00c0",
+    "url": "/static/css/main.1fe49f69.chunk.css"
+  },
+  {
+    "revision": "54d4e60e61fc0bbe502e25cfec5fa302",
+    "url": "/index.html"
+  }
+];
